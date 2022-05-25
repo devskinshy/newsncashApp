d49f2520f329ddfc59eccb0a6769dcfe5021e2ee
skinshy <yyshgk12@gmail.com>
2022-05-25 09:50:03 +0900

    1

---
+++ components/Greeting.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Greeting = () => {
+  return (
+    <View>
+      <Text>Hello Functional Component!</Text>
+    </View>
+  );
+};
+
+export default Greeting;
