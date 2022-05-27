c461937a49b6647ac19f6e2a693bd438b5fcee5b
ilacilac <qkralswl307@gmail.com>
2022-05-27 23:42:35 +0900

    Feat: bookmark UI

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useEffect} from 'react';
 import Detail from '../components/webPage/Detail';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-const DetailScreen = ({route}) => {
+const DetailScreen = ({route, navigation}) => {
+  const test = '20220527.0013208287.001';
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <Icon
+          name="bookmark"
+          size={24}
+          color={route.params.idsk === test ? '#333' : '#f6f6f6'}
+        />
+      ),
+    });
+  }, [navigation, route]);
+
   return <Detail data={route.params} />;
 };
 
