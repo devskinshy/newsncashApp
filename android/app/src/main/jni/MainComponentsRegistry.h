feca3c505b6c268bd161bd1b97f5cd2967e2bbee
anisia20 <anisia20@nate.com>
2022-07-04 15:52:30 +0900

    deploy version update

--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/learnreactnative/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/newsncash/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
